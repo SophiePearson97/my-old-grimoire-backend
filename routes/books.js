36aa513fa777cd3c7c345c594a1d9a70c8f22666
SophiePearson97 <s.pearson97@yahoo.co.uk>
2026-01-26 19:42:37 +0200

    Error fixes to auth

--- routes/books.js
+++ routes/books.js
@@ -5,17 +5,15 @@ const auth = require("../middleware/auth");
 const upload = require("../middleware/multer");
 const bookCtrl = require("../controllers/bookController");
 
-router.use(auth);
-
+// (no auth required)
 router.get("/bestrating", bookCtrl.getBestRating);
-
 router.get("/", bookCtrl.getAll);
 router.get("/:id", bookCtrl.getOne);
 
-router.post("/", upload, bookCtrl.create);
-router.put("/:id", upload, bookCtrl.update);
-router.delete("/:id", bookCtrl.remove);
-
-router.post("/:id/rating", bookCtrl.rate);
+// (auth required)
+router.post("/", auth, upload, bookCtrl.create);
+router.put("/:id", auth, upload, bookCtrl.update);
+router.delete("/:id", auth, bookCtrl.remove);
+router.post("/:id/rating", auth, bookCtrl.rate);
 
 module.exports = router;
